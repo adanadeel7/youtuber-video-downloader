abf40edacbd834f53088f33a5f67e6221c525b77
Adan Adeel <adanadeel903@gmail.com>
2026-07-26 12:08:57 +0500

    Controllers Started

---
+++ Server/Controllers/auth.controllers.ts
@@ -0,0 +1,84 @@
+import mongoose from 'mongoose'
+import bcrypt from "bcrypt"
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv'
+import expres from 'express'
+import { User } from '../models/user.model.ts'
+
+
+dotenv.config()
+
+const cookieOptions = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+};
+
+
+const jwt_secret:any = process.env.JWT_SECRET; 
+
+
+async function Register(req:any,res:any) {
+    try {
+        // take name, email , password from body 
+
+        const {name , email, password} = req.body
+
+
+        // Check weather User Exists 
+        const userExists = await User.findOne(
+            {
+                email
+            }
+        )
+
+        if (userExists) {
+            return res.status(400).json({
+                message:"The User Already Exists"
+            })
+        }
+
+        
+        //Hash Password 
+        const hashedPassword = await bcrypt.hash(password,10)
+        const user = await User.create({
+            name, 
+            email, 
+            password : hashedPassword
+        })
+
+        // Token Creation 
+
+        const token = jwt.sign(
+            {
+                id : user._id
+            }, 
+            jwt_secret, {expiresIn: "7d"}
+        )
+
+        //Cookie Creation 
+        res.cookie("token", token, cookieOptions);
+
+
+        //Success Response 
+        res.status(201).json({
+            message : "User Successfully Registered", 
+            token, 
+            user : { 
+                _id : user._id, 
+                email : user.email, 
+                name : user.name
+            }
+        })
+
+
+
+
+    } catch (error) {
+        return res 
+        .status(500)
+        .json({message : "Registration Failed"})
+    }
+    
+}
--- Server/server.ts
+++ Server/server.ts
@@ -1,9 +1,31 @@
 import express from "express"
+import dotenv from 'dotenv'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
 
+
+
+
+dotenv.config()
 
 const app = express()
 const PORT : number = 8000; 
 
+
+
+
+app.use(cors({
+    origin:process.env.CLIENT_URL || 'http://localhost:5173',
+    credentials: true
+}))
+
+
+app.get('/health', (req,res) => {
+    res.json({
+        status : 'ok', 
+        message : 'Api is Running'
+    })
+})
 
 app.listen(PORT, (err)=> {
     if (err) { 
