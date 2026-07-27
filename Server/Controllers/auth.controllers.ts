2a49c0bebbdb912efd6bddc7df9d4277bc982b27
Adan Adeel <adanadeel903@gmail.com>
2026-07-27 11:46:30 +0500

    Auth Controllers Done

--- Server/Controllers/auth.controllers.ts
+++ Server/Controllers/auth.controllers.ts
@@ -82,3 +82,83 @@ async function Register(req:any,res:any) {
     }
     
 }
+
+async function Login(req:any,res:any) {
+    try {
+        // take name, email , password from body 
+
+        const {name , email, password} = req.body
+
+
+        // Check weather User Exists 
+        const users = await User.findOne(
+            {
+                email
+            }
+        )
+
+        if (!users) {
+            return res.status(400).json({
+                message:"Invalid email or password"
+            })
+        }
+
+        
+        //Hash Password 
+        const isPasswordValid = await bcrypt.compare(password,users.password)
+        
+        if (!isPasswordValid) { 
+            return res.status(400).json(
+                {
+                    message : " Invalid email or password"
+                }
+            )
+        }
+
+        // Token Creation 
+
+        const token = jwt.sign(
+            {
+                id : users._id
+            }, 
+            jwt_secret, {expiresIn: "7d"}
+        )
+
+        //Cookie Creation 
+        res.cookie("token", token, cookieOptions);
+
+
+        //Success Response 
+        res.status(200).json({
+            message : "User Successfully Logined in", 
+            token, 
+            user : { 
+                _id : users._id, 
+                email : users.email, 
+                name : users.name
+            }
+        })
+
+
+
+
+    } catch (error:any) {
+        return res 
+        .status(500)
+        .json({message : "Login Failed", error : error.message})
+    }
+
+    
+}
+
+
+async function logoutUser(req : any,res : any) {
+    res.clearCookie("token")
+    res.status(200).json({
+        message: "User Successfully logged Out"
+    })
+    
+}
+
+
+export {Register, Login, logoutUser}
